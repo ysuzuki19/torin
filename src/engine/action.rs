facb124eed238121a3cdf383d1d3b6bf44193932
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-05-25 02:18:27 +0900

    rename engine to action

---
+++ src/engine/action.rs
@@ -0,0 +1,58 @@
+use super::file;
+use super::mode;
+use super::plan;
+use super::plan::Prune;
+use crate::config;
+use crate::model;
+use crate::prelude::*;
+
+pub struct Action {
+    mode: mode::Mode,
+}
+
+// torin DELETE NEIGHBOR rule=today
+// enum Output {
+//     Json,
+//     Stdout,
+// }
+// struct Args {
+//     output: Output,
+// }
+
+impl Action {
+    pub fn new(mode: mode::Mode) -> Self {
+        Action { mode }
+    }
+
+    pub fn run(&self, ctx: config::context::Context, path: String) -> Result<()> {
+        println!("Action is running!");
+        // 1. List all files in the directory
+        // 2. walk through each file
+        let mut f = file::File::load(&path)?;
+        while let Some(plans) = plan::Plans::parse(f.lines())?.prune(&ctx)? {
+            if plans.all(|p| p.command().is_error()) {
+                plans.iter().for_each(|p| {
+                    println!("{p:?}");
+                });
+                break;
+            }
+
+            let p = plans.first()?;
+            match p.command() {
+                model::Command::Delete => {
+                    f.drain(p.begin(), p.end());
+                }
+                model::Command::Error => {}
+            }
+        }
+        match self.mode {
+            mode::Mode::Plan => f.dump(file::Destination::Stdout)?,
+            mode::Mode::Check => {
+                todo!() //TODO: check if any changes or errors are detected
+            }
+            // mode::Mode::Apply => f.dump(file::Destination::Overwrite)?,
+            mode::Mode::Apply => f.dump(file::Destination::File(format!("{path}.expected")))?,
+        };
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,20 @@ mod prelude;
 
 use clap::Parser;
 
-fn cli() -> localtrace::Result<()> {
+fn main() {
     let cli = config::cli::Cli::parse();
-    let manifest = config::manifest::Manifest::load()?;
-    println!("Manifest.project.includes: {:?}", manifest.project.includes);
-    println!("Manifest.project.excludes: {:?}", manifest.project.excludes);
-    println!("Manifest.project.rules: {:?}", manifest.project.rules);
-    println!("Manifest.project.sources: {:?}", manifest.sources()?);
-
     localtrace::with_trace(|| {
+        let manifest = config::manifest::Manifest::load()?;
+        println!("Manifest.project.includes: {:?}", manifest.project.includes);
+        println!("Manifest.project.excludes: {:?}", manifest.project.excludes);
+        println!("Manifest.project.rules: {:?}", manifest.project.rules);
+        println!("Manifest.project.sources: {:?}", manifest.sources()?);
+
         match cli.mode {
             config::cli::Mode::Plan | config::cli::Mode::Check | config::cli::Mode::Apply => {
-                let ctx = config::context::Context::load(vec!["debug"]);
+                let ctx = config::context::Context::load(manifest.project.rules);
                 let sample_path = "src/e2e/sample.rs".into();
-                engine::Engine::new(cli.mode.try_into()?).run(ctx, sample_path)?;
+                engine::action::Action::new(cli.mode.try_into()?).run(ctx, sample_path)?;
             }
             config::cli::Mode::Completion { shell } => {
                 let mut cmd = <config::cli::Cli as clap::CommandFactory>::command();
@@ -30,14 +30,4 @@ fn cli() -> localtrace::Result<()> {
         }
         Ok(())
     });
-
-    Ok(())
-}
-
-fn main() {
-    let result = cli();
-    if let Err(e) = result {
-        eprintln!("Error: {e}");
-        std::process::exit(1);
-    }
 }
