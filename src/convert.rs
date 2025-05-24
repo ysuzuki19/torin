9081dab4d4a8f902575cfec049af658185400e35
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-05-25 01:43:19 +0900

    refine convert uses

--- src/convert.rs
+++ src/convert.rs
@@ -1,11 +1,12 @@
 use super::prelude::*;
 
-use crate::{config::annotation::*, model};
+use crate::config;
+use crate::model;
 
-impl TryFrom<Params> for Option<model::Trigger> {
+impl TryFrom<config::annotation::Params> for Option<model::Trigger> {
     type Error = Error;
 
-    fn try_from(params: Params) -> Result<Self> {
+    fn try_from(params: config::annotation::Params) -> Result<Self> {
         match (params.rule, params.date) {
             (Some(rule), None) => Ok(Some(model::Trigger::Rule(model::Rule::new(rule)))),
             (None, Some(date)) => Ok(Some(model::Trigger::Date(model::Date::try_from(date)?))),
