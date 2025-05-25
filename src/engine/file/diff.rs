7427a2b3b03079099b7407302b6c131968e66815
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-05-25 18:14:44 +0900

    impl Diff tool

---
+++ src/engine/file/diff.rs
@@ -0,0 +1,85 @@
+pub struct Diff {
+    begin: usize,
+    end: usize,
+    content: String,
+}
+
+impl Diff {
+    pub fn unified_diff_format(&self) -> String {
+        format!(
+            "@@ -{},{} +{},0 @@\n{}",
+            self.begin + 1,
+            self.end - self.begin,
+            self.begin + 1,
+            self.content
+        )
+    }
+}
+
+pub struct DiffBuilder {
+    begin: usize,
+    lines: Vec<String>,
+}
+
+impl DiffBuilder {
+    pub fn new(begin: usize) -> Self {
+        Self {
+            begin,
+            lines: vec![],
+        }
+    }
+
+    pub fn add(&mut self, line: String) {
+        self.lines.push(line);
+    }
+
+    pub fn build(self) -> Diff {
+        Diff {
+            begin: self.begin,
+            end: self.begin + self.lines.len(),
+            content: self
+                .lines
+                .into_iter()
+                .map(|s| format!("- {s}"))
+                .collect::<Vec<_>>()
+                .join("\n"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_diff() {
+        let mut builder = DiffBuilder::new(0);
+        let first_line = "This is a test line.".to_string();
+        builder.add(first_line);
+        let second_line = "This is another line.".to_string();
+        builder.add(second_line);
+
+        assert_eq!(builder.begin, 0);
+        assert_eq!(
+            builder.lines,
+            vec![
+                "This is a test line.".to_string(),
+                "This is another line.".to_string()
+            ]
+        );
+
+        let diff = builder.build();
+        assert_eq!(diff.begin, 0);
+        assert_eq!(diff.end, 2);
+        assert_eq!(
+            diff.content,
+            "- This is a test line.\n- This is another line."
+        );
+
+        let unified_diff = diff.unified_diff_format();
+        assert_eq!(
+            unified_diff,
+            "@@ -1,2 +1,0 @@\n- This is a test line.\n- This is another line."
+        );
+    }
+}
