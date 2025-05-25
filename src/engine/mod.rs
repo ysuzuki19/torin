9e60dc0af5faacc32c2d45a663e41d18f7553ee5
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-05-26 02:03:25 +0900

    $ torin apply

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -19,12 +19,6 @@ impl Engine {
     pub fn init(cli: config::cli::Cli) -> Result<Self> {
         let mode = cli.mode.try_into()?;
         let manifest = config::manifest::Manifest::load()?;
-        // torin DELETE BEGIN rule=manifest-execution
-        println!("Manifest.project.includes: {:?}", manifest.project.includes);
-        println!("Manifest.project.excludes: {:?}", manifest.project.excludes);
-        println!("Manifest.project.rules: {:?}", manifest.project.rules);
-        println!("Manifest.project.sources: {:?}", manifest.sources()?);
-        // torin DELETE END
         let sources = manifest.sources()?;
         let ctx = context::Context::load(manifest.project.rules);
         Ok(Self { mode, ctx, sources })
